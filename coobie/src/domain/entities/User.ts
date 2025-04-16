c2307588afc508d1556ab86f0b3e1e9e211520d3
DKULena <32173057@dankook.ac.kr>
2025-04-16 16:50:34 +0900

    Fix: User속성 테이블 맞게 변경

--- coobie/src/domain/entities/User.ts
+++ coobie/src/domain/entities/User.ts
@@ -1,20 +1,16 @@
 export class User {
-	constructor(
-		public id?: string,
-		public username?: string,
-		public nickname?: string,
-		public password?: string,
-		public departmentId?: number,
-		public positionId?: number,
-		public loginAttempts: number = 0,
-		public isLocked: boolean = false,
-		public isApproved: boolean = false,
-		public status: 'online' | 'offline' | 'busy' | 'away' = 'offline',
-		public profileMessage?: string,
-		public profileImageId?: number,
-		public notificationOn: boolean = true,
-		public roleId?: string,
-		public createdAt?: Date,
-		public deletedAt?: Date
-	) {}
+  constructor(
+    public id: string,
+    public username: string,
+    public nickname: string,
+    public password: string,
+    public isLocked: boolean = false,
+    public createdAt: Date,
+    public isApproved: boolean = false,
+    public notificationOn: boolean = true,
+    public roleId: string,
+    public deletedAt?: Date,
+    public departmentId?: number,
+    public positionId?: number
+  ) {}
 }
