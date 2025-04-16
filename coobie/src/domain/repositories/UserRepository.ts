7bdad41238215caf6be062842ab40f5f017fbf65
DKULena <32173057@dankook.ac.kr>
2025-04-16 16:51:31 +0900

    Fix: 불필요한 메서드 삭제

--- coobie/src/domain/repositories/UserRepository.ts
+++ coobie/src/domain/repositories/UserRepository.ts
@@ -2,17 +2,14 @@ import { User } from "../entities/User";
 import { UserFilter } from "./filters/UserFilter";
 
 export interface UserRepository {
-	count(filter?: UserFilter): Promise<number>;
-	findAll(filter?: UserFilter): Promise<User[]>;
-	findById(id: string): Promise<User | null>;
-	findByUsername(username: string): Promise<User | null>;
-	findByRoleId(roleId: string, filter?: UserFilter): Promise<User[]>;
-	save(user: User): Promise<User>;
-	update(user: User): Promise<User>;
-	delete(id: string): Promise<void>;
-	resetPassword(id: string, defaultPassword: string): Promise<void>;
-	updateLoginAttempts(id: string, attempts: number): Promise<void>;
-	updateLockStatus(id: string, isLocked: boolean): Promise<void>;
-	updateApprovalStatus(id: string, isApproved: boolean): Promise<void>;
-	updateStatus(id: string, status: 'online' | 'offline' | 'busy' | 'away'): Promise<void>;
+  count(filter?: UserFilter): Promise<number>;
+  findAll(filter?: UserFilter): Promise<User[]>;
+  findById(id: string): Promise<User | null>;
+  findByUsername(username: string): Promise<User | null>;
+  findByRoleId(roleId: string, filter?: UserFilter): Promise<User[]>;
+  save(user: User): Promise<User>;
+  update(user: User): Promise<User>;
+  delete(id: string): Promise<void>;
+  resetPassword(id: string, defaultPassword: string): Promise<void>;
+  updateLockStatus(id: string, isLocked: boolean): Promise<void>;
 }
