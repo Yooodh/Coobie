a0039963511a7df03ed9bd14dbc37f1ec7bc49ea
DKULena <32173057@dankook.ac.kr>
2025-04-22 16:22:42 +0900

    Feat: 사업자 번호 parameter 추가

--- coobie/src/application/usecases/user/CreateUserUsecase.ts
+++ coobie/src/application/usecases/user/CreateUserUsecase.ts
@@ -11,11 +11,17 @@ export class CreateUserUseCase {
     password: string,
     departmentId?: number,
     positionId?: number,
-    roleId: string = "02" // 기본값을 "02"로 설정
+    roleId: string = "02", // 기본값을 "02"로 설정
+    businessNumber: string="" // 사업자 번호 추가
   ): Promise<User> {
     const existingUser = await this.userRepository.findByUsername(username);
     if (existingUser) {
       throw new Error("이미 존재하는 사용자명입니다");
+    }
+
+    // 사업자 번호가 비어있으면 에러 발생
+    if (!businessNumber) {
+      throw new Error("사업자 번호는 필수 입력 항목입니다")
     }
 
     const newUser = new User(
@@ -28,7 +34,7 @@ export class CreateUserUseCase {
       false, // isApproved (승인 상태: 기본값 false, 회사 관리자의 승인 필요)
       true, // notificationOn (알림 상태: 기본값 true)
       roleId, // 역할 ID (기본값 "02")
-      "", // businessNumber (선택적 필드로 처리)
+      businessNumber, // 사업자 번호 설정
       undefined, // deletedAt (삭제일: 기본값 undefined)
       departmentId,
       positionId
