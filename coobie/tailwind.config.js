9cdeb72dccdf91fcfbcd6a6f8db22b5be2b5c920
Yooodh <eogus2604@hanmail.net>
2025-04-24 16:01:27 +0900

    "chore: Schedule 애니메이션 추가"

--- coobie/tailwind.config.js
+++ coobie/tailwind.config.js
@@ -2,10 +2,16 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fadeIn: "fadeIn 0.3s ease-in-out",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0, transform: "translateY(10px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
+      },
+    },
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+};
