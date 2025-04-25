066d342141c33fa5ec7f20bda25cdb7114de5e08
DKULena <32173057@dankook.ac.kr>
2025-04-25 14:39:47 +0900

    Feat: DepartmentRepository 인터페이스에 삭제 기능 추가

--- coobie/src/domain/repositories/DepartmentRepository.ts
+++ coobie/src/domain/repositories/DepartmentRepository.ts
@@ -3,5 +3,5 @@ import { Department } from "../entities/Department";
 export interface DepartmentRepository {
   getAllByCompany(companyId: string): Promise<Department[]>;
   create(companyId: string, departmentName: string): Promise<Department>;
-  delete?(id: number): Promise<void>;
+  delete(id: number): Promise<void>;
 }
