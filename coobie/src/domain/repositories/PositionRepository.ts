9528e13b3ec2979f44c2830cdc3104e6cc913405
DKULena <32173057@dankook.ac.kr>
2025-04-25 14:40:07 +0900

    Feat: PositionRepository 인터페이스에 삭제 기능 추가

--- coobie/src/domain/repositories/PositionRepository.ts
+++ coobie/src/domain/repositories/PositionRepository.ts
@@ -3,5 +3,5 @@ import { Position } from "@/domain/entities/Position";
 export interface PositionRepository {
   getAllByCompany(companyId: string): Promise<Position[]>;
   create(companyId: string, positionName: string): Promise<Position>;
-  delete?(id: number): Promise<void>; // 선택적 메서드 (추후 구현)
+  delete(id: number): Promise<void>; // 선택적 메서드 (추후 구현)
 }
