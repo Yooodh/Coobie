ed03c8481bd9d70ef6ccb7f33f35cd3b31e49044
DKULena <32173057@dankook.ac.kr>
2025-04-26 19:44:08 +0900

    Chore: Supabase 이미지 로딩을 위한 remotePatterns 설정 추가

--- coobie/next.config.ts
+++ coobie/next.config.ts
@@ -1,7 +1,39 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   // images: {
+//   //   domains: [
+//   //     'https://xivyzqrqnqznkqqkmhrc.supabase.co'
+//   //   ],
+//   // },
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'https://xivyzqrqnqznkqqkmhrc.supabase.co',
+//         port: '',
+//         pathname: '/storage/v1/object/public/**',
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'xivyzqrqnqznkqqkmhrc.supabase.co',
+        port: '',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
