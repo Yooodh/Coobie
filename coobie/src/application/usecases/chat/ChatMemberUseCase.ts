2454f17d1b626c2c99a77a6a39cd34f114a61298
mjhn010 <mjhn010@naver.com>
2025-04-28 09:58:22 +0900

    "Feat: application/chatting 추가"

--- coobie/src/application/usecases/chat/ChatMemberUseCase.ts
+++ coobie/src/application/usecases/chat/ChatMemberUseCase.ts
@@ -5,13 +5,18 @@ export class ChatMemberUseCase {
   constructor(private chatMemberRepository: ChatRoomMemberRepository) {
     this.chatMemberRepository = chatMemberRepository;
   }
+  async findRoomMember(myId: string) {
+    console.log(myId);
+    const myChatRoom = await this.chatMemberRepository.chatRoomFindAll(myId);
+    return myChatRoom;
+  }
   async execute(chatMember: ChatMemberDto): Promise<ChatMemberDto> {
     // Call the repository to create the chat room
     console.log("chatMember : ", chatMember);
-    const createdMember = await this.chatMemberRepository.addMember({
-      userId: chatMember.userId,
-      chatRoomId: chatMember.chatRoomId,
-    } as ChatMemberDto);
+    const createdMember = await this.chatMemberRepository.addMember(
+      chatMember.chatRoomId,
+      chatMember.userId
+    );
     // Map the returned data to ChatRoomDto
     return createdMember;
   }
